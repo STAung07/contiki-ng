13140bbe5875bb835ba29f84a9ce410309db6623
Le Zong <lezongmun@gmail.com>
2024-04-19 17:06:58 +0800

    CB SWANN

--- examples/nbr/disco.c
+++ examples/nbr/disco.c
@@ -15,7 +15,7 @@
 
 // DISCO Algorithm
 // first, select two prime numbers
-#define DISCO_FIRST_PRIME 3
+#define DISCO_FIRST_PRIME 7
 #define DISCO_SECOND_PRIME 5
 
 // Configures the wake-up timer for neighbour discovery 
@@ -69,9 +69,11 @@ void receive_packet_callback(const void *data, uint16_t len, const linkaddr_t *s
     
     // Copy the content of packet into the data structure
     memcpy(&received_packet_data, data, len);
+    if (received_packet_data.src_id == 5380) {
+      // Print the details of the received packet
+      printf("Received neighbour discovery packet %lu with rssi %d from %ld\r\n", received_packet_data.seq, (signed short)packetbuf_attr(PACKETBUF_ATTR_RSSI),received_packet_data.src_id);
+    }
     
-    // Print the details of the received packet
-    printf("Received neighbour discovery packet %lu with rssi %d from %ld\r\n", received_packet_data.seq, (signed short)packetbuf_attr(PACKETBUF_ATTR_RSSI),received_packet_data.src_id);
   }
 }
 
@@ -84,7 +86,6 @@ void receive_packet_callback(const void *data, uint16_t len, const linkaddr_t *s
 // Scheduler function for the sender of neighbour discovery packets
 char sender_scheduler(struct rtimer *t, void *ptr) {
   static uint16_t i = 0;
-  static int NumSleep=0;
  
   // Begin the protothread
   PT_BEGIN(&pt);
